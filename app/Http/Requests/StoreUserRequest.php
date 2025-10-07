1b2e4a921244b82ed595302453725115a7d964b7
Semen Moiseev <semenmoiseev.2004@mail.ru>
2025-10-07 15:08:50 +0700

    feat(user): add StoreUserRequest

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            // Обязательно | Строка | Длина не больше 255
+            'name'  => ['required', 'string', 'max:255'],
+            // Обязательно | Email | Уникальный в таблице users
+            'email' => ['required', 'email', 'unique:users,email'],
+            // Обязательно | Строка | Минимум 6 символов | Подтвержден
+            'password' => ['required', 'string', 'min:6', 'confirmed']
+        ];
+    }
+}
